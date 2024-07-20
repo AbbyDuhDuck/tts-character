149055284048df1538a07550009d6bd427b130f8
AbbyDuhDuck <abbyduck96@gmail.com>
2024-07-20 01:24:55 -0400

    Made more stable

--- src/service/twitch.py
+++ src/service/twitch.py
@@ -4,6 +4,7 @@
 
 # -=-=- Imports & Globals -=-=- #
 
+import requests
 from twitchAPI.twitch import Twitch
 from twitchAPI.eventsub.base import EventSubBase
 from twitchAPI.object.eventsub import StreamOnlineEvent, StreamOfflineEvent
@@ -12,6 +13,9 @@ from twitchAPI.type import AuthScope, ChatEvent
 from twitchAPI.chat import Chat, EventData, ChatMessage, ChatSub, ChatCommand
 import asyncio
 import secret
+import time
+
+from threading import Thread
 
 APP_ID = secret.twitch.APP_ID
 APP_SECRET = secret.twitch.APP_SECRET
@@ -22,12 +26,39 @@ TARGET_CHANNEL = 'abbyduhduck'
 
 # -=-=- Classes -=-=- #
 
+# class TwitchEventService(EventSubBase):
+#     def __init__(self, twitch: Twitch) -> None:
+#         super().__init__(twitch)
+    
+#     def _build_request_header(self):
+#         return super()._build_request_header()
+    
+#     def _get_transport(self):
+#         return super()._get_transport()
+    
+#     def _subscribe(self, sub_type: str, sub_version: str, condition: dict, callback, event, is_batching_enabled: bool | None = None):
+#         return super()._subscribe(sub_type, sub_version, condition, callback, event, is_batching_enabled)
+    
+#     def _unsubscribe_hook(self, topic_id: str):
+#         return super()._unsubscribe_hook(topic_id)
+    
+#     def start(self):
+#         return super().start()
+    
+#     def stop(self):
+#         return super().stop()
+
 class TwitchService:
     def __init__(self) -> None:
+        self._is_stopping = False
         self._is_connected = False
         self._is_live = False
         self._on_out = []
         self._on_command = []
+        self.token = ''
+        # -=-=- #
+        self.twitch = None
+        self.game_name = ""
 
     async def _init_(self):
         # join our target channel, if you want to join multiple, either call join for each individually
@@ -35,7 +66,49 @@ class TwitchService:
         await self.chat.join_room(TARGET_CHANNEL)
         # you can do other bot initialization things in here
 
+    def is_live(self):
+        if self.token == '' or self.token == None:
+            return False
+
+        headers = {
+            'Client-ID': APP_ID,
+            'Authorization': 'Bearer ' + self.token
+        }
+
+        stream = requests.get('https://api.twitch.tv/helix/streams?stream_type=live&user_login=' + TARGET_CHANNEL, headers=headers)
+        stream_data = stream.json()
+
+        if 'error' in stream_data:
+            msg = f"{stream_data['error']} Error: {stream_data['message']}"
+            self._out(msg, do_print=True)
+            return False
+        elif 'data' in stream_data and len(stream_data['data']) == 1:
+            title = stream_data['data'][0]['title']
+            game = stream_data['data'][0]['game_name']
+            if game != self.game_name:
+                self.game_name = game
+                self._command_(f'!game {game}')
+                print("Live:", title, game)
+            return True
+        return False
+
+    def start_status_reader(self):
+        def main():
+            while not self._is_stopping:
+                is_live = self.is_live()
+                if self._is_live != is_live:
+                    self._is_live = is_live
+                    self._connected(self._is_connected, self._is_live)
+                time.sleep(5)
+        t = Thread(target=main)
+        t.daemon = True
+        t.start()
+
     def start(self):
+        self.start_status_reader()
+        self.start_twitch()
+
+    def start_twitch(self):
         async def main():
             # set up twitch api instance and add user authentication with some scopes
             self.twitch = twitch = await Twitch(APP_ID, APP_SECRET)
@@ -55,19 +128,17 @@ class TwitchService:
             # listen to channel subscriptions
             # chat.register_event(ChatEvent.SUB, on_sub)
             # there are more events, you can view them all in this documentation
-            # events = EventSubBase(twitch)
-
-            # events.listen_stream_online(TARGET_CHANNEL, self._on_stream_online)
-            # events.listen_stream_offline(TARGET_CHANNEL, self._on_stream_offline)
-
+            # events = TwitchEventService(twitch)
+            
             # you can directly register commands and their handlers, this will register the !reply command
             # chat.register_command('reply', test_command)
 
-            # return twitch, chat
             chat.start()
+        # -=-=- #
         asyncio.run(main())
-    
+
     def stop(self):
+        self._is_stopping = True
         self._connected(False, False)
         async def stop():
             print("Stopping Twitch")
@@ -75,19 +146,19 @@ class TwitchService:
             await self.twitch.close()
         asyncio.run(stop())
     
-    def _on_stream_online(self, event: StreamOnlineEvent, _):
-        self._is_live = True
-        self._connected(self._is_connected, self._is_live)
+    # def _on_stream_online(self, event: StreamOnlineEvent, _):
+    #     self._is_live = True
+    #     self._connected(self._is_connected, self._is_live)
 
-    def _on_stream_offline(self, event: StreamOfflineEvent, _):
-        self._is_live = False
-        self._connected(self._is_connected, self._is_live)
+    # def _on_stream_offline(self, event: StreamOfflineEvent, _):
+    #     self._is_live = False
+    #     self._connected(self._is_connected, self._is_live)
 
     def _connected(self, status:bool, is_live:bool):
         self._out(f"!status Twitch {int(status)} {int(is_live)}")
 
     async def _on_ready_event(self, ready_event: EventData):
-        print('Bot is ready for work, joining channels')
+        self._out('Twitch Ready! Joining Channel', do_print=True)
         self._is_connected = True
         self._connected(self._is_connected, self._is_live)
 
@@ -95,22 +166,19 @@ class TwitchService:
         await self._init_()
 
     async def _on_message_event(self, message: ChatMessage):
-        msg = f"Twitch message from {message.user.name}: {message.text}"
-        # TODO: add an on message event
-        print(msg)
-        self._out(msg)
-        self._command_(f"!try_greet {message.user.name}")
+        self._out(f"Twitch message from {message.user.name}: {message.text}", do_print=True)
+        self._command_(f"!greet {message.user.name}")
         # await on_message(shell_out, 'Twitch', msg.user.name, msg.text)
     
     def on_out(self, cb: callable):
         self._on_out.append(cb)
 
-    def _out(self, message: str):
-        print("outing", message, self._on_out)
+    def _out(self, message: str, do_print:bool=False):
+        # print("outing", message, self._on_out)
         if len(self._on_out) == 0:
             print(f"Out: {message}")
         else:
-            for out in self._on_out: out(message)
+            for out in self._on_out: out(message, do_print=do_print)
 
     
     def _command_(self, raw: str):
--- src/service/youtube.py
+++ src/service/youtube.py
@@ -8,9 +8,22 @@ import secret
 import pytchat
 from time import sleep
 
-# from pytchat import LiveChatAsync
+from lxml import html
+import requests
+
+import asyncio
 
 # -=-=- Functions -=-=- #
+
+def get_stream_id(channel_id):
+    page = requests.get(f"https://www.youtube.com/embed/live_stream?channel={channel_id}")
+    if page.status_code == 200:
+        tree = html.fromstring(page.content)
+        links = tree.xpath('//link[@rel="canonical"]')
+        if links:
+            id = links[0].attrib['href'].split("watch?v=")[1]
+            return id
+    return False
 
 # -=-=- Classes -=-=- #
 
@@ -19,42 +32,74 @@ class YouTubeService:
         self._on_out = []
         self._on_command = []
 
+        self._is_connected = False
+        self._is_live = False
+        self._is_stopping = False
+
+        self.chat = None
+        self.video_id = get_stream_id(secret.youtube.CHANNEL_ID)
+
     def start(self):
-        # chat = LiveChatAsync(secret.youtube.VIDEO_ID, callback = func)
-        self.chat = chat = pytchat.create(secret.youtube.VIDEO_ID)
-        self._connected(True, chat.is_replay()) 
+        def main():
+            if not self.video_id:
+                msg = f"Cannot Find YouTube Stream Link for channel ID: {secret.youtube.CHANNEL_ID}"
+                self._out(msg, do_print=True)
+                self.video_id = secret.youtube.VIDEO_ID
+            self._out(f"Found YouTube Stream ID: {self.video_id}", do_print=True)
+            # -=-=- #
+            # chat = LiveChatAsync(secret.youtube.VIDEO_ID, callback = func)
+            # self.chat = chat = pytchat.create(secret.youtube.VIDEO_ID)
+            self.chat = chat = pytchat.create(self.video_id)
+            chat.get()
+            
+            # return livechat
+            while chat.is_alive() and not chat.is_replay():
+                if self._is_connected != chat.is_alive() or self._is_live == chat.is_replay():
+                    self._connected(chat.is_alive(), not chat.is_replay())
+                # if chat.is_replay():
+                #     sleep(5)
+                #     continue
+                # -=-=- #
+                # sync chat
+                for c in chat.get().sync_items():
+                    self._out(f"Youtube message from {c.author.name}: {c.message}", do_print=True)
+                    self._command_(f"!greet {c.author.name}")
+                    # print(f"{c.datetime} [{c.author.name}]- {c.message}")
+                # print("--")
+                sleep(1)
+            self._connected(chat.is_alive(), not chat.is_replay())
+            
+            return not chat.is_replay()
         
-        # return livechat
-        while chat.is_alive() and not chat.is_replay():
-            print("--")
-            # sync chat
-            for c in chat.get().sync_items():
-                msg = f"Youtube message from {c.author.name}: {c.message}"
-                # TODO: add an on message event
-                print(msg)
-                self._out(msg)
-                self._command_(f"!try_greet {c.author.name}")
-                # print(f"{c.datetime} [{c.author.name}]- {c.message}")
-                # await on_message(shell_out, 'YouTube', c.author.name, c.message)
-            sleep(1)
-        self._connected(chat.is_alive(), not chat.is_replay())
+        async def loop():
+            while not self._is_stopping:
+                if not main():
+                    break
+                if not self._is_stopping:
+                    self._out("Restarting YouTube", do_print=True)
+        
+        asyncio.run(loop())
+
 
     def stop(self):
         print("Stopping YouTube")
         self._connected(False, False)
-        self.chat.terminate()
+        if self.chat:
+            self.chat.terminate()
     
     def on_out(self, cb: callable):
         self._on_out.append(cb)
 
-    def _out(self, message: str):
-        print("outing", message, self._on_out)
+    def _out(self, message: str, do_print:bool=False):
+        # print("outing", message, self._on_out)
         if len(self._on_out) == 0:
             print(f"Out: {message}")
         else:
-            for out in self._on_out: out(message)
+            for out in self._on_out: out(message, do_print=do_print)
     
     def _connected(self, status:bool, is_live:bool):
+        self._is_connected = status
+        self._is_live = is_live
         self._out(f"!status YouTube {int(status)} {int(is_live)}")
 
     
